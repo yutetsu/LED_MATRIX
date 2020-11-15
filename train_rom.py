4ab36da2213c63e57ed2d469db3c8804d3c99885
Yutetsudo <yutetsudo@gmail.com>
2020-11-15 21:44:28 +0100

    Update train_rom.py

--- train_rom.py
+++ train_rom.py
@@ -35,59 +35,77 @@ def train(cmd, train, special, type, dest, line, full_type):
                 output[f'SPECIAL'] = special
                 ledframe = 1
                 try:
-                    special_frame = Image.open(
-                        train + "/SPECIAL/" + special + EXT)
+                    special_frame = Image.open(train + "/SPECIAL/" + special + EXT)
                 except IOError:
-                    return f"[{train}] \SPECIA\{special}{EXT} not found"
+                    return f"[{train}] /SPECIA/{special}{EXT} not found"
                 led1.paste(special_frame, (0, 0))
             else:
                 if dest != "" and type == "":
                     output[f'FULL_DEST'] = dest
                     ledframe = 1
                     full_dest_frame = Image.open(
+                    try:
                         train + "/FULL_DEST/" + dest + EXT)
+                    except IOError:
+                        return f"[{train}] /FULL_DEST/{dest}{EXT} not found"
                     led1.paste(full_dest_frame, (0, 0))
                 if line != "" and type == "":
                     output[f'FULL_LINE'] = line
                     ledframe = 1
-                    line_frame = Image.open(train + "/FULL_LINE/" + line + EXT)
+                    try:
+                        line_frame = Image.open(train + "/FULL_LINE/" + line + EXT)
+                    except IOError:
+                        return f"[{train}] /FULL_LINE/{line}{EXT} not found"
                     led1.paste(line_frame, (0, 0))
                 if type != "" and dest == "":
                     output[f'FULL_TYPE'] = type
                     ledframe = 1
-                    full_type_frame = Image.open(
-                        train + "/FULL_TYPE/" + type + EXT)
+                    try:
+                        full_type_frame = Image.open(train + "/FULL_TYPE/" + type + EXT)
+                    except IOError:
+                        return f"[{train}] /FULL_TYPE/{type}{EXT} not found"
                     led1.paste(full_type_frame, (0, 0))
                 if type != "" and dest != "":
                     output[f'TYPE'] = type
                     ledframe = 2
-                    type_jp_frame = Image.open(
-                        train + "/TYPE/" + type + "_JP" + EXT)
-                    type_en_frame = Image.open(
-                        train + "/TYPE/" + type + "_EN" + EXT)
+                    try:
+                        type_jp_frame = Image.open(train + "/TYPE/" + type + "_JP" + EXT)
+                        type_en_frame = Image.open(train + "/TYPE/" + type + "_EN" + EXT)
+                    except IOError:
+                        return f"[{train}] /TYPE/{type}{EXT} not found"
                     led1.paste(type_jp_frame, (0, 0))
                     led2.paste(type_en_frame, (0, 0))
                     output[f'DEST'] = dest
-                    dest_frame = Image.open(train + "/DEST/" + dest + EXT)
+                    try:
+                        dest_frame = Image.open(train + "/DEST/" + dest + EXT)
+                    except IOError:
+                        return f"[{train}] /DEST/{dest}{EXT} not found"
                     led1.paste(dest_frame, (48, 0))
                     led2.paste(dest_frame, (48, 0))
                     if line != "":
                         output[f'LINE'] = line
                         ledframe = 3
-                        line_frame = Image.open(train + "/LINE/" + line + EXT)
+                        try:
+                            line_frame = Image.open(train + "/LINE/" + line + EXT)
+                        except IOError:
+                            return f"[{train}] /LINE/{line}{EXT} not found"
                         led3.paste(type_jp_frame, (0, 0))
                         led3.paste(line_frame, (48, 0))
                         if full_type == "TRUE":
                             output[f'FULL_TYPE'] = full_type
                             ledframe = 4
-                            full_type_frame = Image.open(
-                                train + "/FULL_TYPE/" + type + EXT)
+                            try:
+                                full_type_frame = Image.open(train + "/FULL_TYPE/" + type + EXT)
+                            except IOError:
+                                return f"[{train}] /FULL_TYPE/{type}{EXT} not found"
                             led4.paste(full_type_frame, (0, 0))
                     if full_type == "TRUE":
                         output[f'FULL_TYPE'] = full_type
                         ledframe = 3
-                        full_type_frame = Image.open(
-                            train + "/FULL_TYPE/" + type + EXT)
+                        try:
+                            full_type_frame = Image.open(train + "/FULL_TYPE/" + type + EXT)
+                        except IOError:
+                            return f"[{train}] /FULL_TYPE/{type}{EXT} not found"
                         led3.paste(full_type_frame, (0, 0))
 
         elif train == "RM1000":
@@ -98,18 +116,24 @@ def train(cmd, train, special, type, dest, line, full_type):
                 try:
                     special_frame  = Image.open(train + "/SPECIAL/" + special + EXT)
                 except IOError:
-                    return f"[{train}] \SPECIA\{special}{EXT} not found"
+                    return f"[{train}] /SPECIA/{special}{EXT} not found"
                 led1.paste(special_frame, (0,0))
             else:
                 if type != "":
                     output[f'TYPE'] = type
                     ledframe = 1
-                    type_frame = Image.open(train + "/TYPE/" + type + EXT)
+                    try:
+                        type_frame = Image.open(train + "/TYPE/" + type + EXT)
+                    except IOError:
+                        return f"[{train}] /TYPE/{type}{EXT} not found"
                     led1.paste(type_frame, (0,0))
                 if dest != "":
                     output[f'DEST'] = dest
                     ledframe = 1
-                    dest_frame = Image.open(train + "/DEST/" + dest + EXT)
+                    try:
+                        dest_frame = Image.open(train + "/DEST/" + dest + EXT)
+                    except IOError:
+                        return f"[{train}] /DEST/{dest}{EXT} not found"
                     led1.paste(dest_frame, (48,0))
 
         elif train == "JR305":
@@ -120,18 +144,24 @@ def train(cmd, train, special, type, dest, line, full_type):
                 try:
                     special_frame  = Image.open(train + "/SPECIAL/" + special + EXT)
                 except IOError:
-                    return f"[{train}] \SPECIA\{special}{EXT} not found"
+                    return f"[{train}] /SPECIA/{special}{EXT} not found"
                 led1.paste(special_frame, (0,0))
             else:
                 if type != "":
                     output[f'TYPE'] = type
                     ledframe = 1
-                    type_frame = Image.open(train + "/TYPE/" + type + EXT)
+                    try:
+                        type_frame = Image.open(train + "/TYPE/" + type + EXT)
+                    except IOError:
+                        return f"[{train}] /TYPE/{type}{EXT} not found"
                     led1.paste(type_frame, (0,0))
                 if dest != "":
                     output[f'DEST'] = dest
                     ledframe = 1
-                    dest_frame = Image.open(train + "/DEST/" + dest + EXT)
+                    try:
+                        dest_frame = Image.open(train + "/DEST/" + dest + EXT)
+                    except IOError:
+                        return f"[{train}] /DEST/{dest}{EXT} not found"
                     led1.paste(dest_frame, (52,0))
 
         elif train == "SR01":
@@ -142,18 +172,24 @@ def train(cmd, train, special, type, dest, line, full_type):
                 try:
                     special_frame  = Image.open(train + "/SPECIAL/" + special + EXT)
                 except IOError:
-                    return f"[{train}] \SPECIA\{special}{EXT} not found"
+                    return f"[{train}] /SPECIA/{special}{EXT} not found"
                 led1.paste(special_frame, (0,0))
             else:
                 if type != "":
                     output[f'TYPE'] = type
                     ledframe = 1
-                    type_frame = Image.open(train + "/TYPE/" + type + EXT)
+                    try:
+                        type_frame = Image.open(train + "/TYPE/" + type + EXT)
+                    except IOError:
+                        return f"[{train}] /TYPE/{type}{EXT} not found"
                     led1.paste(type_frame, (0,0))
                 if dest != "":
                     output[f'DEST'] = dest
                     ledframe = 1
-                    dest_frame = Image.open(train + "/DEST/" + dest + EXT)
+                    try:
+                        dest_frame = Image.open(train + "/DEST/" + dest + EXT)
+                    except IOError:
+                        return f"[{train}] /DEST/{dest}{EXT} not found"
                     led1.paste(dest_frame, (48,0))
 
         elif train == "TX3000":
@@ -164,18 +200,24 @@ def train(cmd, train, special, type, dest, line, full_type):
                 try:
                     special_frame  = Image.open(train + "/SPECIAL/" + special + EXT)
                 except IOError:
-                    return f"[{train}] \SPECIA\{special}{EXT} not found"
+                    return f"[{train}] /SPECIA/{special}{EXT} not found"
                 led1.paste(special_frame, (0,0))
             else:
                 if type != "":
                     output[f'TYPE'] = type
                     ledframe = 1
-                    type_frame = Image.open(train + "/TYPE/" + type + EXT)
+                    try:
+                        type_frame = Image.open(train + "/TYPE/" + type + EXT)
+                    except IOError:
+                        return f"[{train}] /TYPE/{type}{EXT} not found"
                     led1.paste(type_frame, (0,0))
                 if dest != "":
                     output[f'DEST'] = dest
                     ledframe = 1
-                    dest_frame = Image.open(train + "/DEST/" + dest + EXT)
+                    try:
+                        dest_frame = Image.open(train + "/DEST/" + dest + EXT)
+                    except IOError:
+                        return f"[{train}] /DEST/{dest}{EXT} not found"
                     led1.paste(dest_frame, (48,0))
 
         elif train == "JR110":
@@ -186,21 +228,27 @@ def train(cmd, train, special, type, dest, line, full_type):
                 try:
                     special_frame  = Image.open(train + "/SPECIAL/" + special + EXT)
                 except IOError:
-                    return f"[{train}] \SPECIA\{special}{EXT} not found"
+                    return f"[{train}] /SPECIA/{special}{EXT} not found"
                 led1.paste(special_frame, (0,0))
             else:
                 if type != "":
                     output[f'TYPE'] = type
                     ledframe = 2
-                    type_frame_jp = Image.open(train + "/TYPE/" + type + "_JP"  + EXT)
-                    type_frame_en = Image.open(train + "/TYPE/" + type + "_EN" + EXT)
+                    try:
+                        type_frame_jp = Image.open(train + "/TYPE/" + type + "_JP"  + EXT)
+                        type_frame_en = Image.open(train + "/TYPE/" + type + "_EN" + EXT)
+                    except IOError:
+                        return f"[{train}] /TYPE/{type}{EXT} not found"
                     led1.paste(type_frame_jp, (0,0))
                     led2.paste(type_frame_en, (0,0))
                 if dest != "":
                     output[f'DEST'] = dest
                     ledframe = 2
-                    dest_frame_jp = Image.open(train + "/DEST/" + dest + "_JP" + EXT)
-                    dest_frame_en = Image.open(train + "/DEST/" + dest + "_EN" + EXT)
+                    try:
+                        dest_frame_jp = Image.open(train + "/DEST/" + dest + "_JP" + EXT)
+                        dest_frame_en = Image.open(train + "/DEST/" + dest + "_EN" + EXT)
+                    except IOError:
+                        return f"[{train}] /DEST/{dest}{EXT} not found"
                     led1.paste(dest_frame_jp, (35,0))
                     led2.paste(dest_frame_en, (35,0))
 
